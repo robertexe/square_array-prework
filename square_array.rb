a133c651d1bce8bbabe10f6abf685af2e84a573f
Robert Tabares <robtabares92@gmail.com>
2018-05-14 18:36:17 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,14 +1,7 @@
-# def square_array(array)
-#   output = []
-#   array.each do | element |
-#     output.push(element**2)
-#   end
-#   return output
-# end
-
-
-def  square_array(array)
+def square_array(array)
+  output = []
   array.each do | element |
-    element = element**2
+    output.push(element**element)
   end
+  return output
 end
